9cf91e4cde88fa731eb006eb39db025dbcd9270a
Marcos Vogado <marcoshcvogado@gmail.com>
2026-08-31 15:30:06 -0300

    excluindo comentarios inadequados

---
+++ CursoMongoDB/Programs/Program_7_3.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using Newtonsoft.Json;
+using CursoMongoDB.Contexts;
+using CursoMongoDB.Services;
+
+namespace CursoMongoDB.Programs;
+
+public static class Program_7_3
+{
+    public static async Task ExecutarAsync()
+    {
+        var configuration = new ConfigurationBuilder()
+            .AddUserSecrets<SecretsAnchor>()
+            .Build();
+
+        var connectionString = configuration["MongoDb:ConnectionString"];
+
+        if (string.IsNullOrWhiteSpace(connectionString))
+        {
+            throw new InvalidOperationException(
+                "Connection string não configurada. Rode: " +
+                "dotnet user-secrets set \"MongoDb:ConnectionString\" \"<sua-string>\"");
+        }
+
+        var contexto = new MongoContext(connectionString, "NoticiasDB");
+        var NoticiaService = new NoticiaService(contexto);
+
+        var Url = "startup_brasileira_lanca_aplicativo_inovador";
+        var comentarioTexto = "Incrível ver jovens tão engajados com ciência!";
+
+        try
+        {
+            await NoticiaService.RemoverComentarioAsync(Url, comentarioTexto);
+            Console.WriteLine("Comentário excluido com sucesso!");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Ocorreu um erro ao excluir o comentário:");
+            Console.WriteLine(ex.Message);
+        }
+
+    }
+
+
+}
